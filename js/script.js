a9e330e6c2c04a2f8bb5d05a79a8d2c78887e46b
TakanoriShima <quark2galaxy@gmail.com>
2022-02-23 13:42:44 +0900

    テキストアニメーション修正

--- js/script.js
+++ js/script.js
@@ -19,21 +19,37 @@ $(function(){
   });
   
   // Topのテキストアニメーション
-  const title = $('h1').text();
-  $('h1').text('','<br><br>');
+  const title_first = $('h1').text();
+  const title_second = $('h2').text();
+  $('h1').text('');
+  $('h2').text('');
   let count = 1;
+  let second_timer;
   // h1のアニメーションの関数を定義
-  const text_animation = () => {
-    const word1 = title.substr(0, count);
+  const first_text_animation = () => {
+    const word1 = title_first.substr(0, count);
     $('h1').text(word1);
     count++;
-    if(count > title.length) {
-      clearInterval(timer);
+    if(count > title_first.length) {
+      clearInterval(first_timer);
+      count = 1;
+      setTimeout(() => {
+        second_timer = setInterval(second_text_animation, 100);
+      }, 1000);
+    }
+  };
+  
+  const second_text_animation = () => {
+    const word2 = title_second.substr(0, count);
+    $('h2').text(word2);
+    count++;
+    if(count > title_second.length) {
+      clearInterval(second_timer);
       count = 1;
     }
   };
   
-  const timer = setInterval(text_animation, 100);
+  const first_timer = setInterval(first_text_animation, 100);
   
 
 });
